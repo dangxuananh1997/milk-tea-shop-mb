217fd29d6fca14b46e87d607fafa36c63b05390f
Dang Xuan Anh <dangxuananh1997@users.noreply.github.com>
2018-07-17 16:50:51 +0700

    add custom font (Source Sans Pro)

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
+import { Font } from 'expo';
+
 import { Provider } from 'react-redux';
 
 import store from './src/store';
 
 import Tab from './src/routes/Tab';
 
+const sourceSanProLight = require('./assets/fonts/SourceSansPro-Light.ttf');
+const sourceSanProRegular = require('./assets/fonts/SourceSansPro-Regular.ttf');
+
 export default class App extends React.Component {
-  componentDidMount() { }
+  componentDidMount() {
+    Font.loadAsync({
+      'source-sans-pro-light': sourceSanProLight,
+      'source-sans-pro-regular': sourceSanProRegular,
+    });
+  }
 
   render() {
     return (
